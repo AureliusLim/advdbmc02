6b21c63c4fe1c55df7309de74d53959c352b0009
SeanCaoile <sean_iverson_caoile@dlsu.edu.ph>
2023-04-12 01:05:15 +0800

    fixed modify,  edited readOne

--- backend/server.js
+++ backend/server.js
@@ -25,33 +25,6 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/index.html'));
 })
 
-// app.get('/centralNode',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'views/centralNode.html'));
-// })
-
-// app.get('/node1',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'views/node1.html'));
-// })
-
-// app.get('/node2',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'views/node2.html'));
-// })
-
-app.get('/editData',(req,res)=>{
-
-
-    
-    //if central node is up
-    res.sendFile(path.join(__dirname,'views/editData.html'));
-
-    //else 
-})
-
-// app.get('/readData',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'views/centralNode.html'));
-// })
-
-
 app.get('/centralread', async(req,res)=>{
         connections.node1.getConnection((err,connection)=>{
             if(err){
@@ -218,6 +191,133 @@ app.get('/centralread', async(req,res)=>{
             }
         })
     
+})
+
+// check if good
+app.get('/centralreadOne', async(req,res)=>{
+    connections.node1.getConnection((err,connection)=>{
+        if(err){
+            // Put error message that server is down
+            let headerString;
+            let header;
+            let arr="";
+            let arr2="";
+            let movieID = req.body.movie_id;
+
+            var query = "SELECT * FROM before1980 WHERE movie_id = " + movieID;
+            var query1 = "SELECT * FROM after1980 WHERE movie_id = " + movieID;
+
+
+            connections.node2.query(query, async(err, result)=>{
+                if(err){
+                    console.log(err)
+                }
+                else if(result){
+                    header = Object.keys(result[0]);
+                    headerString = header.join(",");
+                    arr += headerString;
+                    arr += "\n";
+                    let temp;
+            
+                    // for (let i = 0; i < result.length; i++){
+                    temp = Object.values(result[i]);
+                    temp[1] = temp[1].replaceAll(',','');
+                    temp = temp.toString();
+                    temp += ',';
+                    arr += temp;
+                    arr += "\n";
+                }
+                else{
+                    connections.node3.query(query1, async(err, result)=>{
+                        if(err){
+                            console.log(err)
+                        }
+                        else if(result){
+                            header = Object.keys(result[0]);
+                            headerString = header.join(",");
+                            arr += headerString;
+                            arr += "\n";
+                            let temp;
+                    
+                            // for (let i = 0; i < result.length; i++){
+                            temp = Object.values(result[i]);
+                            temp[1] = temp[1].replaceAll(',','');
+                            temp = temp.toString();
+                            temp += ',';
+                            arr += temp;
+                            arr += "\n";
+                        }
+                        else{
+                            console.log("Movie Id does not exist")
+                        }
+                    })             
+                }
+            })    
+                          
+            try {
+                fs.writeFile(path.resolve(__dirname, 'files', "output.csv"), finalarr, async(err)=>{
+                
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        console.log("saved")
+                        res.download(path.join(__dirname, 'files', "output.csv"))
+                    }
+                });
+                // file written successfully
+            } catch (err) {
+                console.error(err);
+                }
+        }
+        
+        else{
+            // let movieID = req.body.movie_id
+            let movieID = 2
+
+            var query = "SELECT * FROM centraldata WHERE movie_id = " + movieID
+
+            connections.node1.query(query, async(err, result)=>{
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    var header = Object.keys(result[0]);
+                    headerString = header.join(",");
+                    var arr="";
+                    arr += headerString;
+                    arr += "\n";
+                    var temp;
+                    console.log("IN")
+            
+                    let i = 0
+                    temp = Object.values(result[i]);
+                    temp[1] = temp[1].replaceAll(',','');
+                    temp = temp.toString();
+                    temp += ',';
+                    arr += temp;
+                    arr += "\n";
+
+                    try {
+                        fs.writeFile(path.resolve(__dirname, 'files', "output.csv"), arr, (err)=>{
+                            if(err){
+                                console.log(err)
+                            }
+                            else{
+                                console.log("saved")
+                                res.download(path.join(__dirname, 'files', "output.csv"))
+                            }
+                        });
+                        // file written successfully
+                      } catch (err) {
+                        console.error(err);
+                      }
+                }
+                
+            })
+            // res.sendFile(path.join(__dirname,'views/centralNode.html'));
+        }
+    })
 })
 
 app.get('/node2read', async(req,res)=>{
@@ -523,9 +623,6 @@ app.get('/centralinsert', async(req, res)=>{
     let actor1 = req.cookies["actor1"]
     let actor2 = req.cookies["actor2"]
    
-   
-
-   
     var generated_id;
    
             var query = "INSERT INTO centraldata (movie_id, name, year, genre, director_id, actor1, actor2) VALUES(?,?,?,?,?,?,?)";
@@ -648,6 +745,39 @@ app.get('/node3insert', async(req, res)=>{
    
 })
 
+app.post('/connectionstatusMod', (req, res)=>{
+    res.cookie("movieID", req.body.movie_id,{httpOnly:true})
+    res.cookie("movieName", req.body.name,{httpOnly:true})
+    res.cookie("movieYear", req.body.year, {httpOnly:true})
+    res.cookie("movieGenre", req.body.genre, {httpOnly:true})
+    res.cookie("director",req.body.director_id, {httpOnly:true})
+    res.cookie("actor1", req.body.actor1, {httpOnly:true})
+    res.cookie("actor2", req.body.actor2, {httpOnly:true})
+    console.log("inside connection");
+
+    connections.node1.getConnection((err,connection)=>{
+        if(err){
+            //redirect to something else
+            console.log(err)
+
+            //call offline checker
+
+
+            if(Number(movieYear) <= 1980){                    
+                res.redirect('/node2modify')
+            }
+            else{                    
+                res.redirect('/node3modify')
+            }
+        }
+        else{
+           
+            res.redirect('/centralmodify')
+        }
+    
+    })
+})
+
 app.get('/centralmodify', async(req, res)=>{
     // let movieName = req.cookies["movieName"]
     // let movieYear = req.cookies["movieYear"]
@@ -656,30 +786,29 @@ app.get('/centralmodify', async(req, res)=>{
     // let actor1 = req.cookies["actor1"]
     // let actor2 = req.cookies["actor2"]
 
-    let movieID = req.cookies["movieID"]
+    // let movieID = req.cookies["movieID"]
 
-    var movieName = req.cookies["movieName"]
-    var movieYear = req.cookies["movieYear"]
-    var movieGenre = req.cookies["movieGenre"]
-    var director = req.cookies["director"]
-    var actor1 = req.cookies["actor1"]
-    var actor2 = req.cookies["actor2"]
+    // var movieID = req.cookies["movieID"]
+    // var movieName = req.cookies["movieName"]
+    // var movieYear = req.cookies["movieYear"]
+    // var movieGenre = req.cookies["movieGenre"]
+    // var director = req.cookies["director"]
+    // var actor1 = req.cookies["actor1"]
+    // var actor2 = req.cookies["actor2"]
+
+    var movieID = 2
+    var movieName = "$"
+    var movieYear = 1971
+    var movieGenre = "Comedy-Crime"
+    var director = 9970
+    var actor1 = 16703
+    var actor2 = "33054"
 
     //test if good
-        var query = 'UPDATE centraldata SET name = "${movieName}", year = "${movieYear}", genre = "${movieGenre}", director_id = "${director}", actor1 = "${actor1}", actor2="${actor2}" WHERE movie_id = movieID';
-        // var query = "INSERT INTO centraldata (movie_id, name, year, genre, director_id, actor1, actor2) VALUES(?,?,?,?,?,?,?)";
+        var query = "UPDATE centraldata SET name = '" + movieName + "', year = " + movieYear + ", genre = '" + movieGenre + "', director_id = " + director + ", actor1 = " + actor1 + ", actor2 = " + actor2 + " WHERE movie_id = " + movieID;
         
-        // generate movieid     
-        var query2 = "SELECT * from centraldata ORDER BY movie_id DESC LIMIT 1;"
-        connections.node1.query(query2, (err, result)=>{
-            if(err){
-                console.log(err);
-            }
-            else{      
-                console.log(result[0]['movie_id'])
-            }
-        })
-        await new Promise(resolve => setTimeout(resolve, 500));
+        // // generate movieid     
+        // await new Promise(resolve => setTimeout(resolve, 500));
         
         connections.node1.query(query, [movieID, movieName, movieYear, movieGenre, director, actor1, actor2], async(err, result)=>{
             if(err){
@@ -695,6 +824,92 @@ app.get('/centralmodify', async(req, res)=>{
                 else{                    
                     res.redirect('/node3modify')
                 }
+            }
+        })
+})
+
+app.get('/node2modify', async(req, res)=>{
+    // let movieName = req.cookies["movieName"]
+    // let movieYear = req.cookies["movieYear"]
+    // let movieGenre = req.cookies["movieGenre"]
+    // let director = req.cookies["director"]
+    // let actor1 = req.cookies["actor1"]
+    // let actor2 = req.cookies["actor2"]
+
+    // let movieID = req.cookies["movieID"]
+
+    // var movieID = req.cookies["movieID"]
+    // var movieName = req.cookies["movieName"]
+    // var movieYear = req.cookies["movieYear"]
+    // var movieGenre = req.cookies["movieGenre"]
+    // var director = req.cookies["director"]
+    // var actor1 = req.cookies["actor1"]
+    // var actor2 = req.cookies["actor2"]
+
+    var movieID = 2
+    var movieName = "$"
+    var movieYear = 1971
+    var movieGenre = "Comedy-Crime"
+    var director = 9970
+    var actor1 = 16703
+    var actor2 = "33054"
+
+    //test if good
+        var query = "UPDATE before1980 SET name = '" + movieName + "', year = " + movieYear + ", genre = '" + movieGenre + "', director_id = " + director + ", actor1 = " + actor1 + ", actor2 = " + actor2 + " WHERE movie_id = " + movieID;
+        
+        // // generate movieid     
+        // await new Promise(resolve => setTimeout(resolve, 500));
+        
+        connections.node2.query(query, [movieID, movieName, movieYear, movieGenre, director, actor1, actor2], async(err, result)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(result)
+                res.redirect('/editData')
+            }
+        })
+})
+
+app.get('/node3modify', async(req, res)=>{
+    // let movieName = req.cookies["movieName"]
+    // let movieYear = req.cookies["movieYear"]
+    // let movieGenre = req.cookies["movieGenre"]
+    // let director = req.cookies["director"]
+    // let actor1 = req.cookies["actor1"]
+    // let actor2 = req.cookies["actor2"]
+
+    // let movieID = req.cookies["movieID"]
+
+    // var movieID = req.cookies["movieID"]
+    // var movieName = req.cookies["movieName"]
+    // var movieYear = req.cookies["movieYear"]
+    // var movieGenre = req.cookies["movieGenre"]
+    // var director = req.cookies["director"]
+    // var actor1 = req.cookies["actor1"]
+    // var actor2 = req.cookies["actor2"]
+
+    var movieID = 2
+    var movieName = "$"
+    var movieYear = 1971
+    var movieGenre = "Comedy-Crime"
+    var director = 9970
+    var actor1 = 16703
+    var actor2 = "33054"
+
+    //test if good
+        var query = "UPDATE after1980 SET name = '" + movieName + "', year = " + movieYear + ", genre = '" + movieGenre + "', director_id = " + director + ", actor1 = " + actor1 + ", actor2 = " + actor2 + " WHERE movie_id = " + movieID;
+        
+        // // generate movieid     
+        // await new Promise(resolve => setTimeout(resolve, 500));
+        
+        connections.node3.query(query, [movieID, movieName, movieYear, movieGenre, director, actor1, actor2], async(err, result)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(result)
+                res.redirect('/editData')
             }
         })
 })
